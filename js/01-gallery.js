4532aa0ec09a85eff5893713b54b485d6744202b
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-08-13 00:27:24 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMurkup(galleryItems);
+galleryRef.innerHTML = galleryMarkup;
+
+const gallery = new SimpleLightbox('.gallery  a', {
+  scrollZoom: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMurkup(gallery) {
+  return gallery
+    .map(image => {
+      return `
+        <a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+        </a>
+        `;
+    })
+    .join('');
+}
