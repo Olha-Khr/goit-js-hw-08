e7168b90981785733d8a059572f82bc7cff9e017
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-08-13 06:13:26 +0300

    js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,28 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
-// console.log(galleryItems);
 
-const galleryRef = document.querySelector('.gallery');
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
 
-const galleryMarkup = createGalleryMurkup(galleryItems);
-galleryRef.innerHTML = galleryMarkup;
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
 
-const gallery = new SimpleLightbox('.gallery  a', {
-  scrollZoom: false,
-  captionsData: 'alt',
-  captionDelay: 250,
+let gallery = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: '250',
 });
 
-function createGalleryMurkup(gallery) {
-  return gallery
-    .map(image => {
-      return `
-        <a class="gallery__item" href="${image.original}">
-            <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
-        </a>
-        `;
-    })
-    .join('');
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`})
+        .join('')
 }
