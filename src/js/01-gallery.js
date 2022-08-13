0531fbd770ac12e4cb64e8abe125a307010cda59
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-08-13 08:24:40 +0300

    dep

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const refs = {
